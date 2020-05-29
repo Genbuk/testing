e6ce6f7ce377c31b3c8c50d0f3d8ab1f2b2ba6b1
Genbuk <66083336+Genbuk@users.noreply.github.com>
2020-05-28 21:19:58 -0400

    Add files via upload

---
+++ php/enviar.php
@@ -0,0 +1,24 @@
+<?php
+//llamar los campos
+$nombre = $_POST['nombre'];
+$mail = $_POST['correo'];
+$empresa = $_POST['mensaje'];
+
+//Datos del correo
+$para = 'ccampos.geminis@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+//contenido
+$mensaje = "De: $nombre \n";
+$mensaje .= "Correo: $mail \n";
+$mensaje .= "Mensaje: $empresa";
+
+//envio
+mail($para, $asunto, $mensaje);
+header('Location:../index.html');
+
+//$header = 'From: ' . $mail . " \r\n";
+//$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+//$header .= "Mime-Version: 1.0 \r\n";
+//$header .= "Content-Type: text/plain";
+?>
